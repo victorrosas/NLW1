4240a0b90e28c9894c4a064667f2427e2d8d0d05
Victor Rosas <victor.rosas.96@hotmail.com>
2021-03-04 01:03:12 -0500

    created routes specific list

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import knex from './database/connection';
 
 import PointsControllers from './controllers/PointsControllers';
 import ItemsController from './controllers/ItemsControllers';
+
+// index, show, create, update, delete
 
 const routes = express.Router();
 const pointsControllers = new PointsControllers;
@@ -11,6 +12,8 @@ const itemsController = new ItemsController;
 routes.use(express.json());
 
 routes.get('/items', itemsController.index);
+
 routes.post('/points', pointsControllers.create);
+routes.get('/points/:id', pointsControllers.show);
 
 export default routes;
