03f3bf51a0150c5b4440e76d8498f1266ad6a5ae
Victor Rosas <victor.rosas.96@hotmail.com>
2021-03-04 00:05:34 -0500

    first point created

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,48 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Hello World'})
+routes.use(express.json());
+
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return {
+            id: item,
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+        }
+    })
+
+    return response.json(serializedItems)
+});
+
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    await knex('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    });
+
+    return response.json({ success: true});
 });
 
 export default routes;
